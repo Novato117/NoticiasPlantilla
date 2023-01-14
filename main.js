b751e298f2139a5ebecb23ddceb00b08c2f5eb5c
Luis Fernando <93270513+Novato117@users.noreply.github.com>
2023-01-13 18:00:54 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-btn').addEventListener('click',() => {
+       document.querySelector('.nav-menu').classList.toggle('show');
+});
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.productos',{delay:500});
+ScrollReveal().reveal('.carteles',{delay:500});
+ScrollReveal().reveal('.carteles2',{delay:500});
